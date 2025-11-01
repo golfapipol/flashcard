0cb17c0c3d47474c641b9f0e7a6c37f06858f22b
Apipol Sukgler <golf.apipol@gmail.com>
2025-11-01 14:36:46 +0700

    add vercel plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+
+import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vercel(),react()],
   server: {
     port: 3000,
     open: true
